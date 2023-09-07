09b741114539596a821d4509f2172bb5807af28f
Burhan Arslan <49486899+burhanarslan4@users.noreply.github.com>
2023-09-07 06:45:23 +0300

    Delete spring-data-jpa-mappings-master/spring-data-jpa-mappings-master directory

--- spring-data-jpa-mappings-master/spring-data-jpa-mappings-master/src/main/java/com/dev2prod/springdatajpamappings/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dev2prod.springdatajpamappings.repository;
-
-import com.dev2prod.springdatajpamappings.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- spring-data-jpa-mappings-master/spring-data-jpa-mappings-master/src/main/java/com/dev2prod/springdatajpamappings/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dev2prod.springdatajpamappings.repository;
-
-import com.dev2prod.springdatajpamappings.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
